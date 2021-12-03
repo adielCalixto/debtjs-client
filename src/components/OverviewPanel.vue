c7eb9c4ba92f4de5c771566b8951f9507b50116e
adielCalixto <adielsemr@protonmail.com>
2021-12-03 00:52:25 -0300

    debt page new features

---
+++ src/components/OverviewPanel.vue
@@ -0,0 +1,32 @@
+<template>  
+  <div class="artboard shadow-md p-4 my-8 mx-auto max-w-screen-xl">
+    <div class="flex justify-between mb-8">
+        <div class="flex">
+            <slot class="mr-auto" name="header">
+            </slot>
+        </div>
+        <div>
+            <slot name="actions">
+            </slot>
+        </div>
+    </div>
+
+    <slot name="filters">
+    </slot>
+
+    <table class="table w-full">
+        <slot name="table">
+        </slot>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'OverviewPanel',
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Site/Debt.vue
+++ src/pages/Site/Debt.vue
@@ -1,57 +1,149 @@
 <template>
-  <div class="my-10">
-      <div class="mx-auto max-w-3xl shadow-lg p-5">
-        <h2 class="text-2xl mb-10 text-neutral-content">Cadastro de débito</h2>
-        <div class="flex justify-between">
-            <div class="form-control flex-1 mr-2">
-                <label class="label">
-                    <span class="label-text">CPF</span>
-                </label> 
-                <input v-model="pessoa.cpf" type="text" class="input input-bordered">
-            </div> 
-            <div class="form-control">
-                <label class="label">
-                    <span class="label-text">Valor</span>
-                </label>
-                <input v-model="debt.valor" type="text" class="input input-bordered">
-            </div>
+  <OverviewPanel>
+    <template v-slot:header>
+        <span class="text text-5xl">
+            💰
+        </span>
+        <div>
+        <h3 class="text-base-content text-xl text-bold">
+            Dividas
+        </h3>
+        <p class="text-md">Gerencie as dividas</p>
         </div>
-        <button class="btn btn-success mt-10 ml-auto block" @click="getUser">Enviar</button>
-      </div>
-  </div>
+    </template>
+
+    <template v-slot:actions>
+        <button @click="getAll" class="btn btn-outline mr-2">Refresh</button>
+        <button @click="modal = true, currentStep = 0" class="btn btn-success">+ New Debt</button>
+    </template>
+
+    <template v-slot:filters>
+        <Form v-slot="{ handleSubmit }" class="flex gap-4 mb-4">
+            <div class="form-control flex-1">
+                <label class="input-group">
+                    <span>🔍</span>
+                    <Field @change="handleSubmit($event, getFiltered)" type="text" placeholder="Digite o valor..." name="valor" class="input flex-1 input-bordered" />
+                </label>
+            </div>
+            <div class="form-control">
+                <label class="input-group">
+                    <span>Status:</span> 
+                    <Field @change="handleSubmit($event, getFiltered)" as="select" name="status" class="input input-bordered">
+                        <option value="valida">Ativa</option>
+                        <option value="zerada">Inativa</option>
+                        <option value="pendente">Pendente</option>
+                    </Field>
+                </label>
+            </div>
+            <div class="form-control">
+                <label class="input-group">
+                    <span>Data:</span> 
+                    <Field @change="handleSubmit($event, getFiltered)" type="date" name="data" class="input input-bordered" />
+                </label>
+            </div>
+        </Form>
+    </template>
+
+    <template v-slot:table>
+        <thead>
+        <tr>
+            <th></th>
+            <th>Id</th>
+            <th>Valor</th>
+            <th>Status</th>
+            <th>Pessoa</th>
+            <th>Cadastrante</th>
+            <th>Criação</th>
+        </tr>
+        </thead>
+        <tbody>
+            <tr v-for="debt of debts" v-bind:key="debt.divida_id">
+                <td></td>
+                <td>{{ debt.divida_id }}</td>
+                <td>{{ debt.valor }}</td>
+                <td>{{ debt.status }}</td>
+                <td>{{ debt.pessoa_id }}</td>
+                <td>{{ debt.usuario_id }}</td>
+                <td>{{ debt.createdAt }}</td>
+            </tr>
+        </tbody>
+    </template>
+  </OverviewPanel>
+
   <div>
     <div id="my-modal" class="modal" v-bind:class="{'modal-open': modal}">
         <div class="modal-box">
-            <h3 class="text-xl text-base-content">Pessoa inexistente. Realize o cadastro primeiro.</h3>
-            <div>
-                <div class="form-control">
-                    <label class="label">
-                        <span class="label-text">Nome</span>
-                    </label>
-                    <input v-model="pessoa.nome" type="text" class="input input-bordered">
-                </div>
-                <div class="form-control">
-                    <label class="label">
-                        <span class="label-text">CPF</span>
-                    </label>
-                    <input v-model="pessoa.cpf" type="text" class="input input-bordered">
-                </div>
-                <div class="form-control">
-                    <label class="label">
-                        <span class="label-text">Email</span>
-                    </label>
-                    <input v-model="pessoa.email" type="text" class="input input-bordered">
-                </div>
-                <div class="form-control">
-                    <label class="label">
-                        <span class="label-text">Telefone</span>
-                    </label>
-                    <input v-model="pessoa.telefone" type="text" class="input input-bordered">
-                </div>
-            </div>
-            <div class="modal-action"> 
-                <button @click="createUser" class="btn btn-success">Submit</button>
-                <button @click="modal = false" class="btn">Close</button>
+            <Form
+            @submit="nextStep"
+            >
+                <template v-if="currentStep === 0">
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">CPF</span>
+                        </label> 
+
+                        <Field type="text" name="cpf" rules="required" v-model="formData.cpf" class="input input-bordered" />
+                        <ErrorMessage name="cpf" class="error" />
+                    </div> 
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Valor</span>
+                        </label> 
+
+                        <Field type="number" name="valor" rules="required" v-model="formData.valor" class="input input-bordered" />
+                        <ErrorMessage name="valor" class="error" />
+                    </div>
+                </template>
+
+                <template v-if="currentStep === 1">
+                    <div class="mb-4">
+                        <h2 class="text-2xl text-bold">Pessoa inexistente</h2>
+                        <h3 class="text-md text-base-content">Cadastre-a primeiro</h3>
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Nome</span>
+                        </label> 
+
+                        <Field type="text" name="nome" rules="required" v-model="formData.nome" class="input input-bordered" />
+                        <ErrorMessage name="nome" class="error" />
+                    </div> 
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">CPF</span>
+                        </label> 
+
+                        <Field type="text" name="cpf" rules="required" v-model="formData.cpf" class="input input-bordered" />
+                        <ErrorMessage name="cpf" class="error" />
+                    </div> 
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text">Email</span>
+                        </label> 
+
+                        <Field type="email" name="email" rules="required|email" v-model="formData.email" class="input input-bordered" />
+                        <ErrorMessage name="email" class="error" />
+                    </div>
+                </template>
+
+                <template v-if="currentStep === 2">
+                    <div v-if="erro">
+                        <h2 class="text-2xl text-error">{{ erro }}</h2>
+                    </div>
+                    <div v-else>
+                        <h2 class="text-2xl">Confirme os dados:</h2>
+                        <p class="text-md">
+                            Valor: {{ formData.valor }}
+                            CPF: {{ formData.cpf }}
+                        </p>
+                        <button type="submit" class="btn btn-warning btn-block mt-8">Criar</button>
+                    </div>
+                </template>
+
+                <button class="btn btn-outline btn-success btn-block mt-8" v-if="currentStep !== 2" type="submit">Próximo</button>
+            </Form>
+            <div class="modal-action">
+                <button @click="modal = false" class="btn">Cancelar</button>
             </div>
         </div>
     </div>
@@ -59,64 +151,140 @@
 </template>
 
 <script>
+import OverviewPanel from '@/components/OverviewPanel'
+import { Form, Field, ErrorMessage } from 'vee-validate'
 
 export default {
     name: 'Debt',
+    components: {
+        OverviewPanel,
+        Form,
+        Field,
+        ErrorMessage,
+    },
     inject: ['$axios'],
     data: function() {
         return {
+            currentStep: 0,
+            formData: {},
             modal: false,
-            pessoa: {
-                nome: '',
-                email: '',
-                telefone: '',
-                cpf: '',
-                pessoa_id: '',
+            debts: [],
+            query: {
+                limit: 10,
+                offset: 0,
             },
-            debt: {
-                valor: 0,
-            }
+            erro: '',
         }
     },
+    mounted: function() {
+        this.getAll();
+    },
     methods: {
-        createUser: function() {
-            const { nome, email, telefone, cpf } = this.pessoa
-            this.$axios.post('/pessoas/create', { nome, email, telefone, cpf })
-            .then(result => {
-                if(result.data) {
-                    this.pessoa = result.data
-                    this.sendDebt()
-                    this.modal = false
+        nextStep: function(values) {
+            Object.assign(this.formData, values)
+
+            if (this.currentStep === 0) {
+                this.getPessoa(this.formData)
+                .then(response => {
+                    if(response) {
+                        this.formData.pessoa_id = response.pessoa_id
+                        return this.currentStep = 2
+                    }
+                    return this.currentStep = 1
+                })
+                .catch(() => {
+                    console.log("Error ocurred")
+                })
+            }
+
+            if (this.currentStep === 1) {
+                this.createPessoa(this.formData)
+                .then((response) => {
+                    if(response) {
+                        this.formData.pessoa_id = response.pessoa_id
+                        return this.currentStep = 2;
+                    }
+                    this.erro = "Usuario não foi cadastrado."
+                    return this.currentStep = 2;
+                })
+            }
+
+            if (this.currentStep === 2) {
+                this.createDivida(this.formData)
+                .then((response) => {
+                    if(response) {
+                        this.getAll()
+                        return this.modal = false
+                    }
+                    return this.erro = "Divida não foi criada."
+                })
+            }
+        },
+        getPessoa: function({ cpf }) {
+            return this.$axios.get(`/pessoas/${cpf}/cpf`, { cpf })
+            .then(response => {
+                if(response.data) {
+                    return Promise.resolve(response.data)
                 }
+                return Promise.resolve(false)
+            })
+            .catch(() => {
+                console.log('error ocurred')
+                return Promise.reject()
+            })
+        },
+        createPessoa: function({ cpf, nome, email, telefone }) {
+            return this.$axios.post('/pessoas/create', { cpf, nome, email, telefone })
+            .then(response => {
+                if(response.data) {
+                    return Promise.resolve(response.data)
+                }
+                return Promise.resolve(false)
             })
             .catch(() => {
                 console.log('error ocurred')
             })
         },
-        getUser: function() {
-            this.$axios.get(`/pessoas/${this.pessoa.cpf}/cpf`)
-            .then(result => {
-                if(result.data != '') {
-                    this.pessoa = result.data
-                    this.sendDebt()
-                } else {
-                    this.modal = true
+        createDivida: function({ valor, pessoa_id }) {
+            return this.$axios.post('/dividas/create', { valor, pessoa_id })
+            .then(response => {
+                if(response.data) {
+                    return Promise.resolve(response.data)
                 }
+                return Promise.resolve(false)
             })
             .catch(() => {
                 console.log('error ocurred')
             })
         },
-        sendDebt: function() {
-            this.$axios.post('/dividas/create', { valor: this.debt.valor, pessoa_id: this.pessoa.pessoa_id })
-            .then(result => {
-                if(result.data) {
-                    console.log(result.data)
+        getFiltered: function(values) {
+            var esc = encodeURIComponent;
+            var query = Object.keys(values)
+                .map(k => esc(k) + '=' + (values[k] ? esc(values[k]) : ''))
+                .join('&');
+
+            this.$axios.get(`/dividas?${query}&limit=${this.query.limit}&offset=${this.query.offset}`)
+            .then(response => {
+                if(response.data) {
+                    this.debts = response.data
                 }
-                console.log(result)
+                else {
+                    this.debts = []
+                }
             })
             .catch(() => {
-                console.log('error ocurred')
+                console.log("Error ocurred")
+            })
+        },
+        getAll: function() {
+            this.$axios.get(`/dividas?limit=${this.query.limit}&offset=${this.query.offset}`)
+            .then(response => {
+                if(response.data) {
+                    this.debts = response.data
+                }
+            })
+            .catch(() => {
+                console.log("Error ocurred")
             })
         },
     }
